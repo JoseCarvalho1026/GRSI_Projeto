5925bb27ef5b106d8b2385ee3d3227d7fdb2fb0c
JoseCarvalho1026 <gasparcarvalho1026@gmail.com>
2022-03-31 17:39:38 +0000

    HTML Configuration

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//Rota Principal
 app.use(express.static('./public'));
 app.get('/' , (request, response) =>{
     response.sendFile(path.join(__dirname,('./public/index.html')))
-})
+});
 
 const port = 3000;
-app.listen(port)
+app.listen(port, () => {
+    console.log('App listening on port ' + port);
+});
 
 
 /*
